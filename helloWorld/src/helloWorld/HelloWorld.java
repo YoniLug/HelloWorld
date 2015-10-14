8c7ea1f2d59b9b50078b9264d4eb413e58acaeb7
Yoni Lugassi <yonilugassi@gmail.com>
2015-10-14 21:01:38 +0300

    hello project
    simple class

---
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s;
+		s = args[0];
+		System.out.println("Hello World "+s);
+		
+		
+	}
+
+}
